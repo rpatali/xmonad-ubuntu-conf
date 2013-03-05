2ad95523d70ce66e9256969330c65b106954bc4c
Rohit Patali <rohit.patali@locationlabs.com>
2013-03-05 12:26:31 -0800

    added keybindings for lock screen

--- xmonad.hs
+++ xmonad.hs
@@ -201,6 +201,7 @@ myKeyBindings =
     , ((myModMask, xK_z), sendMessage MirrorExpand)
     , ((myModMask, xK_p), spawn "synapse")
     , ((myModMask, xK_u), focusUrgent)
+    , ((myModMask .|. controlMask, xK_l), spawn "xscreensaver-command -lock")
     {-, ((0, 0x1008FF12), spawn "amixer -q set Master toggle")-}
     {-, ((0, 0x1008FF11), spawn "amixer -q set Master 10%-")-}
     {-, ((0, 0x1008FF13), spawn "amixer -q set Master 10%+")-}
